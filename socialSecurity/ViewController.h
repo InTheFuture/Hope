c37b64979110f1759d6de1c1984c4b6b00a4f4d4
Huan Liu <huanliu@HuandeMacBook-Pro.local>
2018-03-24 21:05:51 +0800

    Initial Commit

---
+++ socialSecurity/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  socialSecurity
+//
+//  Created by Huan Liu on 2018/3/24.
+//  Copyright © 2018年 Huan Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ socialSecurity/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  socialSecurity
+//
+//  Created by Huan Liu on 2018/3/24.
+//  Copyright © 2018年 Huan Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
